08d3e34dd7e9dc3341f12a91626b69155cf37d3a
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-02-08 16:19:04 +0200

    custem information student

--- lib/views/answer_screen/screens/answer_screen.dart
+++ lib/views/answer_screen/screens/answer_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/core/resources/color_manager.dart';
+
+import '../widgets/custem_information_students.dart';
 
 class AnswerScreen extends StatelessWidget {
   const AnswerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Map<String, dynamic> data =
-        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
-    print(data);
-    return const Scaffold();
+    // Map<String, dynamic> data =
+    //     ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    return const Scaffold(
+      backgroundColor: ColorManager.kPrimaryColor,
+      body: Column(
+        children: [
+          CustomInformationStudent(
+            name: 'mohamed',
+            grade: '5/5',
+          )
+        ],
+      ),
+    );
   }
 }
